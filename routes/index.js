92d38c1268add7e8f5fcac13551d2828c853876c
Kei Namiri <knamiri@yahoo.com>
2018-04-23 14:43:33 -0700

    Heroku

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,7 @@ const apiRoutes = require("./api");
 // API Routes
 router.use("/api", apiRoutes);
 
-router.use("/", function (req, res) {
-  res.send("./client/build/index.html");
-})
+
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
